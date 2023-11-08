011caf7b1da93d88ecc8063c1141de47589cc6fc
sampurna445 <128822770+sampurna445@users.noreply.github.com>
2023-11-08 16:03:11 +0000

    CryptoHelper

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
 import Navigator from './navigation';
 import {Provider} from 'react-redux';
 import {store} from './store';
 import {AppStateProvider} from './components/AppStateProvider';
+import {AppStateReceiver} from './components/AppStateReceiver';
+import {CryptoHelper} from './helpers';
 
 const App = () => {
+  useEffect(() => {
+    const encryptedString = CryptoHelper.encryptString(
+      'hey this is plain text',
+    );
+
+    console.log('Encrypted String: ' + encryptedString);
+  }, []);
   return (
-    // <Provider store={store}>
-    //   <NavigationContainer>
-    //     <Navigator />
-    //   </NavigationContainer>
-    // </Provider>
     <Provider store={store}>
       <AppStateProvider>
         <NavigationContainer>
--- helpers/index.js
+++ helpers/index.js
@@ -1,3 +1,4 @@
 import ApiHelper from './ApiHelper';
+import CryptoHelper from './CryptoHelper';
 
-export {ApiHelper};
+export {ApiHelper, CryptoHelper};
